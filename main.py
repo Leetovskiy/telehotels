119d0517df4b622c56faca6de32c090007faa78a
Vitaliy Zaitsev <a.leetovskiy@gmail.com>
2021-09-06 21:32:38 +0500

    Add a logging to polling call

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import requests
+from loguru import logger
+
 import handlers
 from loader import bot
 
 if __name__ == '__main__':
-    bot.polling(non_stop=True, interval=1)
+    try:
+        bot.polling(non_stop=True, interval=1)
+    except (requests.ConnectionError, requests.Timeout) as e:
+        logger.error(f'Ошибка соединения во время поллинга: {e}')
+    finally:
+        bot.close()
